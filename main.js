adcd7649d4334e23d8d60c3797fc59bc0a2ee9cf
Chris LoJacono <clojacono92@gmail.com>
2020-07-20 18:52:02 -0500

    tested data and primitive types

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("We start javascript today and I am TERRIFIED")
+
+
+// undefined
+// let x;
+
+
+
+// Boolean
+// let x = true;
+// let y = false;
+// // console.log(x,y)
+
+// if(x){
+//     console.log("this is true")
+// }
+// Number
+// let number = '22';
+// console.log(Number(number) + 22)
+
+
+// // String
+// let string = 'Today is Monday!'
+// console.log(typeof string)
+
